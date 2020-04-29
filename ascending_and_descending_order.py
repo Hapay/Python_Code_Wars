ee329ae899f8b3e4a676cf0eed003d77846789b4
Patryk <xhapay@gmail.com>
2020-04-29 11:19:06 +0200

    Initial commit

---
+++ ascending_and_descending_order.py
@@ -0,0 +1,12 @@
+def descending_order(num):
+    return int("".join(sorted(str(num), reverse=True)))
+
+
+print(descending_order(1247621))
+
+
+def ascending_order(num):
+    return int("".join(sorted(str(num), reverse=True)))
+
+
+print(ascending_order(1247621))
